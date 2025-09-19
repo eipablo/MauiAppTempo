6f5f41e28fb89a9c2a7a0b28d4fec4e89b69c347
Pablo Vinícius <71195004+eipablo@users.noreply.github.com>
2025-09-19 15:17:55 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppTempo/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiAppTempo")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiAppTempo.Pages")]
